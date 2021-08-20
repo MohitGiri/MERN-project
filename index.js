334d8f531ae558e95f280a9df5b2f6eb38755325
MohitGiri <35299968+MohitGiri@users.noreply.github.com>
2021-08-20 20:27:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+const authRoutes = require("./routes/auth")
+const userRoutes = require("./routes/user")
+
+const app = express();
+
+
+
+
+mongoose.connect('mongodb://localhost:27017/csDB', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useCreateIndex: true
+}).then(()=>console.log("DATABASE CONNECTED"));
+
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+
+
+
+
+// Routes
+app.use("/api",authRoutes)
+app.use("/api",userRoutes)
+
+app.listen(3000,()=>{
+    console.log("Server started")
+})
+
+const API = 'http:localhost:3000/api/'
